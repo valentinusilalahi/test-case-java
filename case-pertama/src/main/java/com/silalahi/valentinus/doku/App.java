0032be89671375f348825b1ccfff96b70170906a
valentinus silalahi <valentinus.silalahi@gmail.com>
2018-09-24 21:56:02 +0700

    commit project pertama case nomor satu

---
+++ case-pertama/src/main/java/com/silalahi/valentinus/doku/App.java
@@ -0,0 +1,13 @@
+package com.silalahi.valentinus.doku;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
